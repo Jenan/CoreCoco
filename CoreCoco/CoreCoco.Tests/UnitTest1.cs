712dbccdf4d0e6b6af56bed794945cc6b98eef3a
Jan <jan.skoruba@gmail.com>
2017-09-02 20:34:10 +0200

    Add solution

---
+++ CoreCoco/CoreCoco.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace CoreCoco.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void FakeTest1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void FakeTest2()
+        {
+            Assert.False(false);
+        }
+    }
+}
